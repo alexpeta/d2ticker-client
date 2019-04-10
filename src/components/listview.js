07965f2fe3b82ea5b7bbc1a25b92fe227d466176
Alex Peta <alex.peta@macys.com>
2019-04-10 16:36:53 -0400

    misc

--- src/components/listview.js
+++ src/components/listview.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './listview.css';
 import { ListItem } from './listitem';
+import './listview.css';
 
 export class ListView extends React.Component {
   render() {
     return (
       <div className="list-container">
         {this.props.items.map(c => (
-          <ListItem value={c.value} display={c.display} />
+          <ListItem key={c.value} display={c.display} />
         ))}
       </div>
     );
