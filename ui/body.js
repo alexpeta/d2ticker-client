fd979244b4c0db9a40ded02a9080436de3c73055
Alex Peta <alex.peta@macys.com>
2019-04-10 10:20:10 -0400

    small refactoring

--- ui/body.js
+++ ui/body.js
@@ -1,21 +1,4 @@
 const html = require('choo/html');
-// const Promo = require('./promo');
-// const Header = require('./header');
-// const Footer = require('./footer');
-
-// function banner(state) {
-//   if (state.layout) {
-//     return; // server side
-//   }
-//   const show =
-//     !state.capabilities.standalone &&
-//     !/firefox/i.test(navigator.userAgent) &&
-//     document.querySelector('html').lang.startsWith('en') &&
-//     !state.route.startsWith('/unsupported/');
-//   if (show) {
-//     return state.cache(Promo, 'promo').render();
-//   }
-// }
 
 module.exports = function body(main) {
   return function(state, emit) {
