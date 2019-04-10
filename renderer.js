9b347807c058480ee5e5d486376c4439ff187726
Alex Peta <alex.peta@macys.com>
2019-04-10 08:02:33 -0400

    made it pretty

--- renderer.js
+++ renderer.js
@@ -2,37 +2,37 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-var html = require('choo/html')
-//var devtools = require('choo-devtools')
-var choo = require('choo')
+var html = require('choo/html');
+var devtools = require('choo-devtools');
+var choo = require('choo');
 
-var app = choo()
+var app = choo();
 
 if (process.env.NODE_ENV !== 'production') {
-    app.use(require('choo-devtools')());
+  app.use(require('choo-devtools')());
 }
 
-app.use(countStore)
-app.route('/', mainView)
-app.mount('body')
+app.use(countStore);
+app.route('/', mainView);
+app.mount('body');
 
-function mainView (state, emit) {
+function mainView(state, emit) {
   return html`
     <body>
       <h1>count is ${state.count}</h1>
       <button onclick=${onclick}>Increment</button>
     </body>
-  `
+  `;
 
-  function onclick () {
-    emit('increment', 1)
+  function onclick() {
+    emit('increment', 1);
   }
 }
 
-function countStore (state, emitter) {
-  state.count = 0
-  emitter.on('increment', function (count) {
-    state.count += count
-    emitter.emit('render')
-  })
+function countStore(state, emitter) {
+  state.count = 0;
+  emitter.on('increment', function(count) {
+    state.count += count;
+    emitter.emit('render');
+  });
 }
